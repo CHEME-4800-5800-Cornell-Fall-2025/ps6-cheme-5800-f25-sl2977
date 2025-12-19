04aa165174d7bc3f6791a6650a1894c4d535abfd
sl2977 <sl2977@cornell.edu>
2025-12-19 17:07:40 -0500

    update types.jl

--- src/Types.jl
+++ src/Types.jl
@@ -24,9 +24,8 @@ mutable struct MyExperimentalDrugCocktailContext <: MyAbstractContextModel
     cost::Dict{Int, Float64}      # maps drug type to cost per mg/kg
     levels::Dict{Int, NamedTuple} # maps drug level to drug concentration in mg/kg
     W::Float64             # weight of the patient in kg
-
-    # TODO: you need to add a safety field, and a bounds field here
-    throw(ErrorException("You need to add a safety field, and a bounds field here"));
+    S::Int64               # safety limit for total dosage
+    bounds::Array{Float64,2}      # bounds matrix (K x 2) with min/max for each drug
 
     # constructor -
     MyExperimentalDrugCocktailContext() = new(); # create new *empty* instance 
